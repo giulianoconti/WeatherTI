407de053c7fe78e4418455391f5c5618b395930f
GiuliannT <giuliconti1@gmail.com>
2023-04-04 13:13:08 -0300

    vercel.json added

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Weathers } from "./pages/Weathers";
@@ -6,18 +7,13 @@ import { Error404 } from "./pages/Error404";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <Routes>
-      {/* <Route path="/" element={<Weathers />} /> 
-      <Route path="/:lat/:lon/latlon" element={<WeatherSelected />} />
-      <Route path="*" element={<Error404 />} /> */}
-      <Route path="/" element={<div>hola ruta home</div>} />
-      <Route path="/test" element={<div>hola ruta test</div>} />
-      <Route path="*" element={<div>hola ruta 404</div>} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Weathers />} />
+        <Route path="/:lat/:lon/latlon" element={<WeatherSelected />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-/*  <React.StrictMode>
-    <App />
-  </React.StrictMode>, */
