0bcb07259bf02cb122a29feec899066d91361720
GiuliannT <giuliconti1@gmail.com>
2023-04-04 11:20:39 -0300

    --vh removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Weathers } from "./pages/Weathers";
 import { WeatherSelected } from "./pages/WeatherSelected";
 import { Error404 } from "./pages/Error404";
 import "./App.css";
 
-function calculateViewportHeight() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-}
-
 export default function App() {
-  useEffect(() => {
-    calculateViewportHeight();
-    window.addEventListener("resize", calculateViewportHeight);
-    return () => {
-      window.removeEventListener("resize", calculateViewportHeight);
-    };
-  }, []);
-
   return (
     <BrowserRouter>
       <Routes>
