64dcac4f0b49a4fb416338755ade3010b083d9de
GiuliannT <giuliconti1@gmail.com>
2023-04-04 10:56:04 -0300

    test changes

--- src/components/WeatherSelectedComponents/SunriseGraph.tsx
+++ src/components/WeatherSelectedComponents/SunriseGraph.tsx
@@ -26,7 +26,7 @@ export const SunriseGraph = ({ sunrise = "06:00 AM", sunset = "08:00 PM" }: { su
         <path
           d={`M 0 60 C 14 60 30 60 ${sunriseHour * 6.25} 35 C ${(sunriseHour * 6.25 + (sunsetHour * 6.25 + 75)) / 2 - 12.5} 0 ${
             (sunriseHour * 6.25 + (sunsetHour * 6.25 + 75)) / 2 + 12.5
-          } 0 ${sunsetHour * 6.25 + 75} 35 C 145 60 150 60 150 60`}
+          } 0 ${sunsetHour * 6.25 + 75} 35 C 130 60 150 60 150 60`}
           fill="none"
           stroke="rgb(255, 255, 255)"
           strokeWidth="3"
--- src/pages/WeatherSelected.tsx
+++ src/pages/WeatherSelected.tsx
@@ -5,6 +5,7 @@ import { DataWeather } from "../interfaces/Interfaces";
 import { SunriseGraph } from "../components/WeatherSelectedComponents/SunriseGraph";
 import { imagesBgDay, imagesBgNight, imagesDay, imagesNight, imagesSvg } from "../components/Images";
 import "./WeatherSelected.css";
+import { WindGraph } from "../components/WeatherSelectedComponents/WindGraph";
 
 export const WeatherSelected = (): JSX.Element => {
   const infoRef = useRef<HTMLDivElement>(null);
@@ -61,30 +62,6 @@ export const WeatherSelected = (): JSX.Element => {
     const hours12 = hour % 12 || 12;
     return hours12 + " " + ampm;
   };
-
-  const getWindDirectionStyle = (windDir: string): { left: string; rotate: string; top: string } => {
-    const directionMapping: Record<string, { left: string; rotate: string; top: string }> = {
-      E: { left: "30%", rotate: "90deg", top: "0%" },
-      ENE: { left: "27.5%", rotate: "67.5deg", top: "-11.5%" },
-      ESE: { left: "27.5%", rotate: "112.5deg", top: "11.5%" },
-      N: { left: "0%", rotate: "0deg", top: "-30%" },
-      NE: { left: "21%", rotate: "45deg", top: "-21%" },
-      NNE: { left: "11.5%", rotate: "22.5deg", top: "-27.5%" },
-      NNW: { left: "-11.5%", rotate: "-22.5deg", top: "-27.5%" },
-      NW: { left: "-21%", rotate: "-45deg", top: "-21%" },
-      S: { left: "0%", rotate: "180deg", top: "30%" },
-      SE: { left: "21%", rotate: "135deg", top: "21%" },
-      SSE: { left: "11.5%", rotate: "157.5deg", top: "27.5%" },
-      SSW: { left: "-11.5%", rotate: "-157.5deg", top: "27.5%" },
-      SW: { left: "-21%", rotate: "-135deg", top: "21%" },
-      W: { left: "-30%", rotate: "-90deg", top: "0%" },
-      WNW: { left: "-27.5%", rotate: "-67.5deg", top: "-11.5%" },
-      WSW: { left: "-27.5%", rotate: "-112.5deg", top: "11.5%" },
-    };
-    return directionMapping[windDir] || directionMapping["N"];
-  };
-
-  const windDirectionStyle = getWindDirectionStyle(dataWeather.current.wind_dir);
 
   return (
     <div
@@ -199,25 +176,7 @@ export const WeatherSelected = (): JSX.Element => {
                 <img className="weatherSelected_widget_icon" src={imagesSvg.wind} alt="Weather Icon" />
                 <h3>WIND</h3>
               </div>
-              <div className="weatherSelected_widget_graph_compass">
-                <h3>N</h3>
-                <h3>W</h3>
-                <h3>E</h3>
-                <h3>S</h3>
-                <h2>{dataWeather.current.wind_kph}</h2>
-                <h4>km/h</h4>
-                <h5>{dataWeather.current.wind_dir}</h5>
-                <img
-                  className="weatherSelected_widget_wind_arrow"
-                  src={imagesSvg.arrow_up}
-                  alt="Weather Icon"
-                  style={{
-                    left: windDirectionStyle.left,
-                    rotate: windDirectionStyle.rotate,
-                    top: windDirectionStyle.top,
-                  }}
-                />
-              </div>
+              <WindGraph dataWeather={dataWeather} />
             </div>
             <div className="weatherSelected_widget">
               <div className="weatherSelected_widget_title">
