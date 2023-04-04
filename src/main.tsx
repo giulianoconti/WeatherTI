282c3b4df53e37aa539636ad59b9681111c631a5
GiuliannT <giuliconti1@gmail.com>
2023-04-04 12:25:21 -0300

    test routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,19 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Weathers } from "./pages/Weathers";
+import { WeatherSelected } from "./pages/WeatherSelected";
+import { Error404 } from "./pages/Error404";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Weathers />} />
+      <Route path="/:lat/:lon/latlon" element={<WeatherSelected />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 /*  <React.StrictMode>
     <App />
